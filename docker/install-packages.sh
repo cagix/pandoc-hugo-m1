85990575aa3069b655f7c5076704e213b3b5a620
Carsten Gips <cagix@fh-bielefeld.de>
2022-01-19 10:46:01 +0100

    upgrade to pandoc 2.17.0.1

--- docker/install-packages.sh
+++ docker/install-packages.sh
@@ -26,5 +26,5 @@ apt-get -q --no-allow-insecure-repositories update              \
 
 
 ## Pandoc and Hugo
-wget https://github.com/jgm/pandoc/releases/download/2.17/pandoc-2.17-1-arm64.deb           && dpkg -i pandoc*.deb && rm pandoc*.deb
+wget https://github.com/jgm/pandoc/releases/download/2.17.0.1/pandoc-2.17.0.1-1-arm64.deb   && dpkg -i pandoc*.deb && rm pandoc*.deb
 wget https://github.com/gohugoio/hugo/releases/download/v0.92.0/hugo_0.92.0_Linux-ARM64.deb && dpkg -i hugo*.deb   && rm hugo*.deb
