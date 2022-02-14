8fc79ddc161b0c9e4c48d8aef7821567c4ace52b
Carsten Gips <cagix@fh-bielefeld.de>
2022-02-14 06:57:12 +0100

    Upgrade version (hugo)

--- docker/install-packages.sh
+++ docker/install-packages.sh
@@ -27,4 +27,4 @@ apt-get -q --no-allow-insecure-repositories update              \
 
 ## Pandoc and Hugo (linux/arm64)
 wget https://github.com/jgm/pandoc/releases/download/2.17.1.1/pandoc-2.17.1.1-1-arm64.deb   && dpkg -i pandoc*.deb && rm pandoc*.deb
-wget https://github.com/gohugoio/hugo/releases/download/v0.92.1/hugo_0.92.1_Linux-ARM64.deb && dpkg -i hugo*.deb   && rm hugo*.deb
+wget https://github.com/gohugoio/hugo/releases/download/v0.92.2/hugo_0.92.2_Linux-ARM64.deb && dpkg -i hugo*.deb   && rm hugo*.deb
