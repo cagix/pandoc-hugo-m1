8ab08d800c1d9ce31059602986a4cd028625cfc7
Carsten Gips <cagix@fh-bielefeld.de>
2022-02-23 06:46:34 +0100

    add url of latest releases (#3)

--- docker/install-packages.sh
+++ docker/install-packages.sh
@@ -25,6 +25,9 @@ apt-get -q --no-allow-insecure-repositories update              \
     && rm -rf /var/lib/apt/lists/*
 
 
-## Pandoc and Hugo (linux/arm64)
+## Pandoc and Hugo (linux/arm64/deb)
+## https://github.com/jgm/pandoc/releases/latest/
 wget https://github.com/jgm/pandoc/releases/download/2.17.1.1/pandoc-2.17.1.1-1-arm64.deb   && dpkg -i pandoc*.deb && rm pandoc*.deb
+
+## https://github.com/gohugoio/hugo/releases/latest/
 wget https://github.com/gohugoio/hugo/releases/download/v0.92.2/hugo_0.92.2_Linux-ARM64.deb && dpkg -i hugo*.deb   && rm hugo*.deb
